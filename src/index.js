0cda11a8aaedb068e9e89ad4cb2a0aa808e52836
Alcven <167204307+Alcven@users.noreply.github.com>
2024-05-10 23:31:42 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const { Client, IntentsBitField, ActivityType } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+let status = [
+  {
+    name: 'на твои булки Олежа',
+    type: ActivityType.Watching,
+    url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  },
+];
+
+client.on('ready', (c) => {
+  console.log(`✅ ${c.user.tag} is online.`);
+
+  setInterval(() => {
+    let random = Math.floor(Math.random() * status.length);
+    client.user.setActivity(status[random]);
+  }, 10000);
+});
+
+client.login(
+  'process.env.TOKEN'
+);
